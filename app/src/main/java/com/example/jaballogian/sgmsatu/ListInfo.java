018ca2afdb695b6373f8bdbf957ea22d51ed5b49
Jabal Logian <jaballogianrma77@gmail.com>
2018-11-08 22:12:16 +0700

    mirip dengan branch semua-fitur-kecuali-home tapi list info yang ini bisa read image

--- app/src/main/java/com/example/jaballogian/sgmsatu/ListInfo.java
+++ app/src/main/java/com/example/jaballogian/sgmsatu/ListInfo.java
@@ -1,36 +1,37 @@
 package com.example.jaballogian.sgmsatu;
 
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 
 public class ListInfo {
 
     private String judul, isi;
-    //private Image gambar;
+    private Drawable gambar;
 
     ListInfo(){
 
     }
 
-    public ListInfo (String judul, String isi){
-
-        this.judul = judul;
-        this.isi = isi;
-    }
-
-//    public ListInfo (Image gambar, String judul, String isi){
+//    public ListInfo (String judul, String isi){
 //
-//        this.gambar = gambar;
 //        this.judul = judul;
 //        this.isi = isi;
 //    }
 
-//    public Image getGambar() {
-//        return gambar;
-//    }
-//
-//    public void setGambar(Image gambar) {
-//        this.gambar = gambar;
-//    }
+    public ListInfo (Drawable gambar, String judul, String isi){
+
+        this.gambar = gambar;
+        this.judul = judul;
+        this.isi = isi;
+    }
+
+    public Drawable getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(Drawable gambar) {
+        this.gambar = gambar;
+    }
 
     public String getIsi() {
         return isi;
